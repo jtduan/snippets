7e8cf1f4d75cdec9a7b84650e9e01ed0f1c9a172
jtduan <jtduan@qq.com>
2017-06-13 17:27:18 +0800

    正则表达式

---
+++ src/main/java/cn/jtduan/snippets/regex/RegexExamples.java
@@ -0,0 +1,24 @@
+package cn.jtduan.snippets.regex;
+
+/**
+ * Created by jintaoduan on 17/6/13.
+ */
+public class RegexExamples {
+
+    /**
+     * (?i)  忽略大小写
+     * (?s)  .可以匹配换行符
+     * (?m)	 ^和$可以匹配一行的开始和结束
+     */
+
+    /**
+     * ?可以实现非贪婪匹配
+     */
+
+    /**
+     * \\1 可以匹配第一个分组
+     * String.replaceAll可以使用$1代理第一个分组的内容
+     */
+    public void matchMultipleLines() {
+    }
+}
